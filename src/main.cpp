7b261e0798ef7b36dfb86ec224af6fcf49eb00c4
Mason Jacob <masonjacob@uri.edu>
2025-11-01 21:23:51 -0400

    volume bug fixes and transition from interrupts to polling

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,7 @@ struct TrackMapping
 };
 
 // Maximum number of tracks in each folder
-#define NUM_UI_FILES 7     // Number of files in UI folder (01)
+#define NUM_UI_FILES 8     // Number of files in UI folder (01)
 #define NUM_VOICE_FILES 1  // Number of files in Voice folder (02)
 #define NUM_MUSIC_FILES 4  // Number of files in Music folder (03)
 #define NUM_CANDIDS_FILES 1 // Number of files in Candids folder (04)
@@ -72,7 +72,10 @@ const TrackMapping SOUNDS[NUM_UI_FILES] = {
     {"music_mode", 4},
     {"voice_mode", 5},
     {"candids_mode", 6},
-    {"settings_mode", 7}};
+    {"settings_mode", 7},
+    {"tone3", 8} // Placeholder for future use
+  };
+
 
 DFRobotDFPlayerMini DFPlayer;
 void printDetail(uint8_t type, int value);
@@ -161,14 +164,14 @@ void enterSettingsMode()
 {
   previousMode = currentMode;
   currentMode = MODE_SETTINGS;
-  Serial.println(F("Entered SETTINGS mode"));
+  // Serial.println(F("Entered SETTINGS mode"));
   playUISound("settings_mode");
 }
 
 void exitSettingsMode()
 {
   currentMode = previousMode;
-  Serial.println(F("Exited SETTINGS mode"));
+  // Serial.println(F("Exited SETTINGS mode"));
   // play menu close sound if defined
   switch (currentMode)
   {
@@ -212,17 +215,17 @@ void changePlaybackMode()
   {
   case MODE_VOICE:
     currentMode = MODE_MUSIC;
-    Serial.println(F("Switched to MUSIC mode"));
+    // Serial.println(F("Switched to MUSIC mode"));
     playUISound("music_mode");
     break;
   case MODE_MUSIC:
     currentMode = MODE_CANDIDS;
-    Serial.println(F("Switched to CANDIDS mode"));
+    // Serial.println(F("Switched to CANDIDS mode"));
     playUISound("candids_mode");
     break;
   case MODE_CANDIDS:
     currentMode = MODE_VOICE;
-    Serial.println(F("Switched to VOICE mode"));
+    // Serial.println(F("Switched to VOICE mode"));
     playUISound("voice_mode");
     break;
   default:
@@ -253,7 +256,7 @@ void replayLastTrack()
     }
     else
     {
-      USBSerial.println(F("No last track to replay"));
+      // USBSerial.println(F("No last track to replay"));
     }
   }
 }
@@ -275,11 +278,11 @@ void togglePlayPause()
     {
       DFPlayer.start();
       isPlaying = true;
-      USBSerial.println(F("Resumed"));
+      // USBSerial.println(F("Resumed"));
     }
     else
     {
-      USBSerial.println(F("No track to resume"));
+      // USBSerial.println(F("No track to resume"));
     }
   }
 }
@@ -295,7 +298,7 @@ void toggleSettingsMode()
   {
     // Save configuration (placeholder) and exit
     // If persistent storage needed, write to EEPROM here.
-    Serial.println(F("Saving configuration and exiting settings mode"));
+    // Serial.println(F("Saving configuration and exiting settings mode"));
     exitSettingsMode();
   }
 }
@@ -303,13 +306,13 @@ void toggleSettingsMode()
 void increaseVolume()
 {
   DFPlayer.volumeUp();
-  playFolderTrack(UI, 2); // Play tone1 as feedback
+  playFolderTrack(UI, 8); // Play tone1 as feedback
 }
 
 void decreaseVolume()
 {
   DFPlayer.volumeDown();
-  playFolderTrack(UI, 2); // Play tone2 as feedback
+  playFolderTrack(UI, 8); // Play tone2 as feedback
 }
 
 void button1ISR()
@@ -366,21 +369,18 @@ void setup()
 {
   // Initialize USB serial for debugging
   USBSerial.begin(USB_SERIAL_BAUD);
-  USBSerial.println(F("Initializing..."));
+  // USBSerial.println(F("Initializing..."));
 
   FPSerial.begin(FP_SERIAL_BAUD); // Hardware serial for DFPlayer
 
-  USBSerial.println(F("Serial ports initialized"));
+  // USBSerial.println(F("Serial ports initialized"));
   // Serial.println(F("DFRobot DFPlayer Mini Demo"));
   // Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
 
   if (!DFPlayer.begin(FPSerial, /*isACK = */ true, /*doReset = */ true))
   { // Use serial to communicate with mp3.
     USBSerial.println(F("Unable to begin:"));
-    USBSerial.println(F("1.Please recheck the connection!"));
-    USBSerial.println(F("2.Please insert the SD card!"));
-    while (true)
-      ;
+    while (true);
   }
   USBSerial.println(F("DFPlayer Mini online."));
 
@@ -416,19 +416,28 @@ void setup()
 
   if (button1.supportsInterrupt())
   {
-    button1.enableInterrupt(button1ISR);
+    // Avoid enabling hardware interrupts for EasyButton here.
+    // Calling button.read() (and the registered onPressed callbacks) from an ISR
+    // can cause the callbacks to run in interrupt context. Those callbacks
+    // perform DFPlayer/Serial operations which are blocking and not safe inside
+    // an ISR, causing the program to freeze. We will use polling in loop()
+    // (button.update()) which runs in the main context and is safe.
+    // button1.enableInterrupt(button1ISR);
     // USBSerial.println("Button will be used through interrupts");
   }
 
   if (button2.supportsInterrupt())
   {
-    button2.enableInterrupt(button2ISR);
+    // See note above: do not enable interrupts to avoid running callbacks
+    // (which call DFPlayer/Serial) inside an ISR. Use polling via update().
+    // button2.enableInterrupt(button2ISR);
     // USBSerial.println("Button will be used through interrupts");
   }
 
   if (button3.supportsInterrupt())
   {
-    button3.enableInterrupt(button3ISR);
+    // See note above for button1/button2.
+    // button3.enableInterrupt(button3ISR);
     // USBSerial.println("Button will be used through interrupts");
   }
   // Play startup sound from UI folder
@@ -438,14 +447,17 @@ void setup()
 
 void loop()
 {
-  button1.update();
-  button2.update();
-  button3.update();
+  button1.read();
+  button2.read();
+  button3.read();
+  // button1.update();
+  // button2.update();
+  // button3.update();
   // handleSerialCommands();
-  if (DFPlayer.available())
-  {
-    printDetail(DFPlayer.readType(), DFPlayer.read()); // Print the detail message from DFPlayer to handle different errors and states.
-  }
+  // if (DFPlayer.available())
+  // {
+  //   printDetail(DFPlayer.readType(), DFPlayer.read()); // Print the detail message from DFPlayer to handle different errors and states.
+  // }
 }
 
 // Serial command handling moved out of loop() for clarity
@@ -478,11 +490,11 @@ void handleSerialCommands()
             int track = atoi(arg);
             DFPlayer.play(track);
             Serial.print(F("CMD: play "));
-            Serial.println(track);
+            // Serial.println(track);
           }
           else
           {
-            Serial.println(F("ERR: play requires a track number"));
+            // Serial.println(F("ERR: play requires a track number"));
           }
         }
         // playfolder <folder> <file>
@@ -498,42 +510,42 @@ void handleSerialCommands()
             Serial.print(F("CMD: playfolder "));
             Serial.print(folder);
             Serial.print(' ');
-            Serial.println(file);
+            // Serial.println(file);
           }
           else
           {
-            Serial.println(F("ERR: playfolder requires folder and file"));
+            // Serial.println(F("ERR: playfolder requires folder and file"));
           }
         }
         // next
         else if (strcmp(token, "next") == 0)
         {
           DFPlayer.next();
-          Serial.println(F("CMD: next"));
+          // Serial.println(F("CMD: next"));
         }
         // prev | previous
         else if (strcmp(token, "prev") == 0 || strcmp(token, "previous") == 0)
         {
           DFPlayer.previous();
-          Serial.println(F("CMD: previous"));
+          // Serial.println(F("CMD: previous"));
         }
         // pause
         else if (strcmp(token, "pause") == 0)
         {
           DFPlayer.pause();
-          Serial.println(F("CMD: pause"));
+          // Serial.println(F("CMD: pause"));
         }
         // resume | start
         else if (strcmp(token, "resume") == 0 || strcmp(token, "start") == 0)
         {
           DFPlayer.start();
-          Serial.println(F("CMD: start/resume"));
+          // Serial.println(F("CMD: start/resume"));
         }
         // stop
         else if (strcmp(token, "stop") == 0)
         {
           DFPlayer.stop();
-          Serial.println(F("CMD: stop"));
+          // Serial.println(F("CMD: stop"));
         }
         // volume <0-30>
         else if (strcmp(token, "volume") == 0 || strcmp(token, "vol") == 0)
@@ -548,24 +560,24 @@ void handleSerialCommands()
               v = 30;
             DFPlayer.volume(v);
             Serial.print(F("CMD: volume "));
-            Serial.println(v);
+            // Serial.println(v);
           }
           else
           {
-            Serial.println(F("ERR: volume requires a value 0-30"));
+            // Serial.println(F("ERR: volume requires a value 0-30"));
           }
         }
         // volup
         else if (strcmp(token, "volup") == 0 || strcmp(token, "volumeup") == 0)
         {
           DFPlayer.volumeUp();
-          Serial.println(F("CMD: volumeUp"));
+          // Serial.println(F("CMD: volumeUp"));
         }
         // voldown
         else if (strcmp(token, "voldown") == 0 || strcmp(token, "volumedown") == 0)
         {
           DFPlayer.volumeDown();
-          Serial.println(F("CMD: volumeDown"));
+          // Serial.println(F("CMD: volumeDown"));
         }
         // eq <normal|pop|rock|jazz|classic|bass>
         else if (strcmp(token, "eq") == 0)
@@ -587,18 +599,18 @@ void handleSerialCommands()
               DFPlayer.EQ(DFPLAYER_EQ_BASS);
             else
             {
-              Serial.println(F("ERR: unknown eq value"));
+              // Serial.println(F("ERR: unknown eq value"));
               a = NULL;
             }
             if (a)
             {
               Serial.print(F("CMD: eq "));
-              Serial.println(a);
+              // Serial.println(a);
             }
           }
           else
           {
-            Serial.println(F("ERR: eq requires a value"));
+            // Serial.println(F("ERR: eq requires a value"));
           }
         }
         // loopfolder <n>
@@ -610,46 +622,46 @@ void handleSerialCommands()
             int f = atoi(a);
             DFPlayer.loopFolder(f);
             Serial.print(F("CMD: loopFolder "));
-            Serial.println(f);
+            // Serial.println(f);
           }
           else
           {
-            Serial.println(F("ERR: loopfolder requires a folder number"));
+            // Serial.println(F("ERR: loopfolder requires a folder number"));
           }
         }
         // sleep
         else if (strcmp(token, "sleep") == 0)
         {
           DFPlayer.sleep();
-          Serial.println(F("CMD: sleep"));
+          // Serial.println(F("CMD: sleep"));
         }
         // reset
         else if (strcmp(token, "reset") == 0)
         {
           DFPlayer.reset();
-          Serial.println(F("CMD: reset"));
+          // Serial.println(F("CMD: reset"));
         }
         // status - read some info
         else if (strcmp(token, "status") == 0)
         {
           Serial.print(F("State: "));
-          Serial.println(DFPlayer.readState());
+          // Serial.println(DFPlayer.readState());
           Serial.print(F("Volume: "));
-          Serial.println(DFPlayer.readVolume());
+          // Serial.println(DFPlayer.readVolume());
           Serial.print(F("EQ: "));
-          Serial.println(DFPlayer.readEQ());
+          // Serial.println(DFPlayer.readEQ());
           Serial.print(F("CurrentFile: "));
-          Serial.println(DFPlayer.readCurrentFileNumber());
+          // Serial.println(DFPlayer.readCurrentFileNumber());
         }
         // help
         else if (strcmp(token, "help") == 0)
         {
-          Serial.println(F("Supported commands: play <n>, playfolder <f> <n>, next, prev, pause, resume, stop, volume <0-30>, volup, voldown, eq <normal|pop|rock|jazz|classic|bass>, loopfolder <n>, sleep, reset, status"));
+          // Serial.println(F("Supported commands: play <n>, playfolder <f> <n>, next, prev, pause, resume, stop, volume <0-30>, volup, voldown, eq <normal|pop|rock|jazz|classic|bass>, loopfolder <n>, sleep, reset, status"));
         }
         else
         {
           Serial.print(F("ERR: unknown command: "));
-          Serial.println(token);
+          // Serial.println(token);
         }
       }
     }
@@ -669,55 +681,55 @@ void printDetail(uint8_t type, int value)
   switch (type)
   {
   case TimeOut:
-    Serial.println(F("Time Out!"));
+    // Serial.println(F("Time Out!"));
     break;
   case WrongStack:
-    Serial.println(F("Stack Wrong!"));
+    // Serial.println(F("Stack Wrong!"));
     break;
   case DFPlayerCardInserted:
-    Serial.println(F("Card Inserted!"));
+    // Serial.println(F("Card Inserted!"));
     break;
   case DFPlayerCardRemoved:
-    Serial.println(F("Card Removed!"));
+    // Serial.println(F("Card Removed!"));
     break;
   case DFPlayerCardOnline:
-    Serial.println(F("Card Online!"));
+    // Serial.println(F("Card Online!"));
     break;
   case DFPlayerUSBInserted:
-    Serial.println("USB Inserted!");
+    // Serial.println("USB Inserted!");
     break;
   case DFPlayerUSBRemoved:
-    Serial.println("USB Removed!");
+    // Serial.println("USB Removed!");
     break;
   case DFPlayerPlayFinished:
     Serial.print(F("Number:"));
     Serial.print(value);
-    Serial.println(F(" Play Finished!"));
+    // Serial.println(F(" Play Finished!"));
     break;
   case DFPlayerError:
     Serial.print(F("DFPlayerError:"));
     switch (value)
     {
     case Busy:
-      Serial.println(F("Card not found"));
+      // Serial.println(F("Card not found"));
       break;
     case Sleeping:
-      Serial.println(F("Sleeping"));
+      // Serial.println(F("Sleeping"));
       break;
     case SerialWrongStack:
-      Serial.println(F("Get Wrong Stack"));
+      // Serial.println(F("Get Wrong Stack"));
       break;
     case CheckSumNotMatch:
-      Serial.println(F("Check Sum Not Match"));
+      // Serial.println(F("Check Sum Not Match"));
       break;
     case FileIndexOut:
-      Serial.println(F("File Index Out of Bound"));
+      // Serial.println(F("File Index Out of Bound"));
       break;
     case FileMismatch:
-      Serial.println(F("Cannot Find File"));
+      // Serial.println(F("Cannot Find File"));
       break;
     case Advertise:
-      Serial.println(F("In Advertise"));
+      // Serial.println(F("In Advertise"));
       break;
     default:
       break;
